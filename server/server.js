25206831a3b0b906d93346d1d4ea662847e3a214
ywy-w <yyngwong@gmail.com>
2022-04-09 17:12:50 -0400

    successfully getting github response with user info
    Co-authored-by: Matt Femia <mattfemia1@gmail.com>
    Co-authored-by: Mireille <Mireille13@users.noreply.github.com>
    Co-authored-by: Eric <ejhagen@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const cors = require('cors');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 require('dotenv').config();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
 const signupRouter = require(path.join(__dirname, './routers/signupRouter'))
 
