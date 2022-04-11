e2a280699d0b153b3500da69e0e04c6005519e0f
ywy-w <yyngwong@gmail.com>
2022-04-10 20:21:55 -0400

    auth is already breaking on me...checking dev

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,19 +25,51 @@ authController.getToken = async (req, res, next) => {
 
 authController.getProfile = async (req, res, next) => {
   const url = 'https://api.github.com/user';
+  console.log(req.cookies);
+  console.log(req.cookies.auth_token)
   try {
     const profileInfoJSON = await fetch(url, {
       method: 'GET',
-      headers: {
-        authorization: `token ${req.cookies.auth_token}`,
-      },
+      headers: { authorization: `token ${req.cookies.auth_token}` },
     });
     const profileInfo = await profileInfoJSON.json();
     res.locals.profile = profileInfo;
+    console.log('profile', res.locals.profile);
     return next();
   } catch (err) {
     return next({
       log: `Error in authController.getProfile Err: ${err.message}`,
+      status: 500,
+      message: { err: 'An error occurred' },
+    });
+  }
+};
+
+authController.getLanguages = async (req, res, next) => {
+  try {
+    console.log('is auth available?', req.cookies.auth_token);
+    const username = res.locals.profile.login;
+    console.log('res.locals', res.locals);
+    // adding page=n to query will give page n as a result
+    // mirror:false, per_page=100
+    const query = encodeURIComponent(`user:${username}`);
+    const url = `https://api.github.com/search/repositories?q={${query}}`;
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        // authorization: `token ${req.cookies.auth_token}`,
+        accept: 'application/vnd.github.v3+json',
+      },
+    });
+    console.log('getLang response', response);
+    const repos = await response.json();
+    console.log('getLang after await .json()', repos);
+    res.locals.repos = repos;
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `Error in authController.getLanguages Err: ${err.message}`,
       status: 500,
       message: { err: 'An error occurred' },
     });
